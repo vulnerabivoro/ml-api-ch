ebff4b8e9ea10fa58b41e94fd07a50332015156e
Debian <admin@ip-172-31-80-107.ec2.internal>
2023-04-13 23:44:25 +0000

    Persistencia ADD RULE sqlite3

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sqlite3
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -11,13 +12,18 @@ def add_yara_rule():
     # Validar que los datos del body sean correctos
     if not name or not rule:
         return jsonify({'error': 'Los datos del body son incorrectos'}), 400
+
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
     
     # Agregar la regla YARA
     # Código para agregar la regla YARA
-    
+    cur.execute('INSERT INTO yara_rules (name, rule) VALUES (?, ?)', (name, rule))
+    conn.commit()
+    cur.close()
+
     # Retornar la respuesta
     return jsonify({'message': 'Regla YARA agregada correctamente'}), 201
-
 
 if __name__ == '__main__':
     app.run(debug=True)
