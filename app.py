db0a63fbc00d06ac4939f5dbc3bc35b0663e6bce
Debian <admin@ip-172-31-80-107.ec2.internal>
2023-04-13 22:37:09 +0000

    Importar los módulos necesarios para crear una API con Python. Utilizaremos Flask. Creacion de instancia flask. Creacion de primer endpoint para agregar regla yara.

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/rule', methods=['POST'])
+def add_yara_rule():
+    # Obtener los datos del body
+    name = request.json['name']
+    rule = request.json['rule']
+    
+    # Validar que los datos del body sean correctos
+    if not name or not rule:
+        return jsonify({'error': 'Los datos del body son incorrectos'}), 400
+    
+    # Agregar la regla YARA
+    # Código para agregar la regla YARA
+    
+    # Retornar la respuesta
+    return jsonify({'message': 'Regla YARA agregada correctamente'}), 201
